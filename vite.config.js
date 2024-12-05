dc7d0b3b531be88a4056a87cfdc83535f20c7d7f
themickymix <micz.yalon@gmail.com>
2024-12-05 15:15:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+      "Permissions-Policy": "not-allowed=()", // Remove or adjust the header
+    },
+  },
+});
