2ae8b267579709dbefd1a99d29622327ea63eff6
themickymix <micz.yalon@gmail.com>
2024-12-05 15:26:25 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+// Import the required plugins using ES Module syntax
+import tailwindcssAnimated from "tailwindcss-animated";
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animated"), require("daisyui")],
+  plugins: [tailwindcssAnimated, daisyui],
 };
